a0816c3a216065c25871921b2b3352caf5910668
mrobin88 <ratthewrobin@gmail.com>
2019-08-07 13:30:29 -0700

    add animation and mole img

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const moleHole = { 
-     1: 'green',
+     1: 'black',
      0: 'transparent'
     };
 // Game Start variables.
-const bonkLimit = 3;
+const bonkLimit = 1;
 let bonks = 0;
 let score = 0; 
 let molesMade = 0;
@@ -17,11 +17,27 @@ let makeItStop = null;
 const init = function(){    
     molesMade = 0;
     makeItStop = setInterval(() => {
+        
         makeMole();
         render();
     },2000);
 }
-//let currentPercent = null;
+
+function moleClicked(){
+    if(board[event.target.id] === 0) {
+        score-=2;
+    }else if(board[event.target.id] === 1){
+        score+=1
+        bonks+=1
+        if (bonks === bonkLimit){
+            bonks = 0;
+            makeMole();
+        }
+    }    
+    render();
+}
+
+//element ref targets
 const holes = document.querySelector('div.mole-land');
 
 const startBtn = document.getElementById('start'); 
@@ -38,24 +54,16 @@ holes.addEventListener('click', function(event){
 startBtn.addEventListener('click', init);
 
     
-function moleClicked(){
-    if(board[event.target.id] === 0) {
-        score-=2;
-    }else if(board[event.target.id] === 1){
-        score+=1
-        bonks+=1
-        if (bonks === bonkLimit){
-            makeMole();
-        }
-    }    
-    render();
-}
+
 
 let render = function(){
+
     board.forEach(function(value, index) {
         document.getElementById(index).style.backgroundColor = moleHole[value];
     });
 score.innerHTML = `Score: ${score}`;
+
+
 
 //display score
 }
@@ -64,27 +72,22 @@ const gameEnd = function(){
     if(score > molesMade){
     scoreBoard.innerHTML = `high score: ${score}  Title earned SLAYER OF THE UNDERWORLD!`
     }else if(score === molesMade){
-        scoreBoard = ('href', 'https://careers.servicemaster.com/en-US/page/terminix  ' ` good score: ${score} Terminex is hiring.`)
+        scoreBoard.innerHTML = `good score: ${score} Terminex is hiring. https://careers.servicemaster.com/en-US/page/terminix `
     }else if(score === 0){
         scoreBoard.innerHTML = `0 moles: You are a saint, a friend of the moles. The mole god is pleased.`
     }else{scoreBoard.innerHTML = `score: ${score} suggested read: How to Wack-A-Mole. for dummies.`
 }
 }
 
-
-
 const makeMole = function(){
-    bonks = 0;
+    
     let moleLocation = Math.floor(Math.random() * 9); // 2
     if(molesMade >= gameModeMaxMoles){
         console.log(`moles made = ${molesMade} and clicks registered = ${clicks}.`)
         clearInterval(makeItStop);
         board.fill(0)
-        gameEnd();
         render();
         // should be moved to render function.
-       
-
     }else if (board[moleLocation] === 1){
         makeMole();
     } else {
@@ -96,14 +99,6 @@ const makeMole = function(){
     render();
     }
 }
-
-
-
-
-
-
-
-
 
 //this is a intervalcounter for keyframe animations
 // var showPercent = window.setInterval(function() {
